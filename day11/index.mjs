d35f04cedb10a03a1a2766ff36654eb6658ec1c0
Martin Dimitrov <martin@sachmata.com>
2022-12-11 11:14:24 +0200

    Day 11 with parse

--- day11/index.mjs
+++ day11/index.mjs
@@ -1,3 +1,5 @@
+import { fromFile } from '../lib/readlines.mjs';
+
 console.log('Day 11');
 
 function gcd(a, b) {
@@ -8,93 +10,76 @@ function lcm(a, b) {
     return (a * b) / gcd(a, b);
 }
 
-const monkeys_example = () => [
-    {
-        items: [79, 98],
-        operation: item => item * 19,
-        test: item => (!(item % 23) ? 2 : 3),
-        inspected: 0,
-    },
-    {
-        items: [54, 65, 75, 74],
-        operation: item => item + 6,
-        test: item => (!(item % 19) ? 2 : 0),
-        inspected: 0,
-    },
-    {
-        items: [79, 60, 97],
-        operation: item => item * item,
-        test: item => (!(item % 13) ? 1 : 3),
-        inspected: 0,
-    },
-    {
-        items: [74],
-        operation: item => item + 3,
-        test: item => (!(item % 17) ? 0 : 1),
-        inspected: 0,
-    },
-];
+function parse(file) {
+    const _monkeys = [];
+    let _lcm = 1;
 
-const lcm_example = [23, 19, 13, 17].reduce((acc, i) => lcm(acc, i), 1);
+    for (let line of fromFile(file)) {
+        if (!line.length) {
+            continue;
+        }
 
-const monkeys_input = () => [
-    {
-        items: [74, 73, 57, 77, 74],
-        operation: item => item * 11,
-        test: item => (!(item % 19) ? 6 : 7),
-        inspected: 0,
-    },
-    {
-        items: [99, 77, 79],
-        operation: item => item + 8,
-        test: item => (!(item % 2) ? 6 : 0),
-        inspected: 0,
-    },
-    {
-        items: [64, 67, 50, 96, 89, 82, 82],
-        operation: item => item + 1,
-        test: item => (!(item % 3) ? 5 : 3),
-        inspected: 0,
-    },
-    {
-        items: [88],
-        operation: item => item * 7,
-        test: item => (!(item % 17) ? 5 : 4),
-        inspected: 0,
-    },
-    {
-        items: [80, 66, 98, 83, 70, 63, 57, 66],
-        operation: item => item + 4,
-        test: item => (!(item % 13) ? 0 : 1),
-        inspected: 0,
-    },
-    {
-        items: [81, 93, 90, 61, 62, 64],
-        operation: item => item + 7,
-        test: item => (!(item % 7) ? 1 : 4),
-        inspected: 0,
-    },
-    {
-        items: [69, 97, 88, 93],
-        operation: item => item * item,
-        test: item => (!(item % 5) ? 7 : 2),
-        inspected: 0,
-    },
-    {
-        items: [59, 80],
-        operation: item => item + 6,
-        test: item => (!(item % 11) ? 2 : 3),
-        inspected: 0,
-    },
-];
+        if (line.startsWith('Monkey')) {
+            _monkeys.push({ inspected: 0 });
+            continue;
+        }
 
-const lcm_input = [19, 2, 3, 17, 13, 7, 5, 11].reduce((acc, i) => lcm(acc, i), 1);
+        const m = _monkeys.at(-1);
 
-// const _monkeys = monkeys_example();
-const _monkeys1 = monkeys_input();
+        if (line.startsWith('  Starting items:')) {
+            m.items = line.substring(18).split(',').map(Number);
+            continue;
+        }
+
+        if (line.startsWith('  Operation:')) {
+            const [o, v] = line.substring(23).split(' ');
+            const _v = +v;
+
+            if (o === '+') {
+                if (!Number.isNaN(_v)) {
+                    m.operation = i => i + _v;
+                } else {
+                    m.operation = i => i + i;
+                }
+            } else if (o === '*') {
+                if (!Number.isNaN(_v)) {
+                    m.operation = i => i * _v;
+                } else {
+                    m.operation = i => i * i;
+                }
+            } else {
+                throw new Error(`Unknown monkey operation '${o}'`);
+            }
+
+            continue;
+        }
+
+        if (line.startsWith('  Test:')) {
+            m.div = +line.substring(21);
+            _lcm = lcm(_lcm, m.div);
+            continue;
+        }
+
+        if (line.startsWith('    If true:')) {
+            m.destA = +line.substring(29);
+            continue;
+        }
+
+        if (line.startsWith('    If false:')) {
+            m.destB = +line.substring(30);
+            continue;
+        }
+    }
+
+    return { monkeys: _monkeys, lcm: _lcm };
+}
+
+const _file = './day11/input.txt';
+
+const _data1 = parse(_file);
 
 for (let round = 1; round <= 20; round++) {
-    for (let monkey of _monkeys1) {
+    for (let monkey of _data1.monkeys) {
         while (monkey.items.length) {
             const item = monkey.items.shift();
             monkey.inspected++;
@@ -102,50 +87,47 @@ for (let round = 1; round <= 20; round++) {
             let worry = monkey.operation(item);
             worry = Math.floor(worry / 3);
 
-            _monkeys1[monkey.test(worry)].items.push(worry);
+            const dest = !(worry % monkey.div) ? monkey.destA : monkey.destB;
+            _data1.monkeys[dest].items.push(worry);
         }
     }
 
     // console.log(
     //     round,
-    //     _monkeys.map(m => m.inspected)
+    //     _data1.monkeys.map(m => m.inspected)
     // );
 }
 
-const [a1, b1] = _monkeys1.sort((a, b) => b.inspected - a.inspected);
+const [a1, b1] = _data1.monkeys.sort((a, b) => b.inspected - a.inspected);
 console.log('Monkey business', a1.inspected * b1.inspected);
 
 console.log('Part two');
 
-// reset
-// _monkeys = monkeys_example();
-const _monkeys2 = monkeys_input();
-
-// const _lcm = lcm_example;
-const _lcm = lcm_input;
+const _data2 = parse(_file);
 
 for (let round = 1; round <= 10000; round++) {
-    for (let monkey of _monkeys2) {
+    for (let monkey of _data2.monkeys) {
         while (monkey.items.length) {
             const item = monkey.items.shift();
             monkey.inspected++;
 
             let worry = monkey.operation(item);
-            worry = worry % _lcm;
+            worry = worry % _data2.lcm;
 
-            _monkeys2[monkey.test(worry)].items.push(worry);
+            const dest = !(worry % monkey.div) ? monkey.destA : monkey.destB;
+            _data2.monkeys[dest].items.push(worry);
         }
     }
 
     // if (!(round % 1000)) {
     //     console.log(
     //         round,
-    //         _monkeys.map(m => m.inspected)
+    //         _data2.monkeys.map(m => m.inspected)
     //     );
     // }
 }
 
-const [a2, b2] = _monkeys2.sort((a, b) => b.inspected - a.inspected);
+const [a2, b2] = _data2.monkeys.sort((a, b) => b.inspected - a.inspected);
 console.log('Monkey business', a2.inspected * b2.inspected);
 
 console.log('End');
