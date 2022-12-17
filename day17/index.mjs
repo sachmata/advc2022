21f477a83f919ad9a799829e7bf08706248efbb6
Martin Dimitrov <martin@sachmata.com>
2022-12-17 22:43:34 +0200

    Day 17 Part two

--- day17/index.mjs
+++ day17/index.mjs
@@ -59,7 +59,6 @@ const CHAMBER_WIDE = 7;
 const EMPTY_CHAMBER_LAYER = '.'.repeat(CHAMBER_WIDE).split('');
 
 let chamber = [];
-let layersTrimmed = 0;
 
 let chamberTop = -1; // null
 
@@ -74,14 +73,11 @@ function draw() {
     console.log('c', chamberTop, 't', rockTop, 'l', rockLeft);
 
     for (let y = chamber.length - 1; y >= 0; y--) {
-        const absolute = String(y + layersTrimmed).padStart(4, '0');
-        const relative = String(y).padStart(3, '0');
+        const absolute = String(y).padStart(4, '0');
 
-        console.log(`${absolute}(${relative}) |${chamber[y].join('')}|`);
+        console.log(`${absolute} |${chamber[y].join('')}|`);
     }
-    if (!layersTrimmed) {
-        console.log(`          +${'-'.repeat(CHAMBER_WIDE)}+`);
-    }
+    console.log(`     +${'-'.repeat(CHAMBER_WIDE)}+`);
     console.log(``);
 }
 
@@ -142,8 +138,6 @@ function step() {
 
     if (jet === '<') {
         const canMoveLeft = rockLayers.every(l => {
-            rockLayers;
-
             const layer = chamber[l];
             const left = layer.indexOf('@');
             return layer[left - 1] === '.';
@@ -205,19 +199,12 @@ function step() {
         });
 
         chamberTop = Math.max(chamberTop, rockTop);
+        chamber = chamber.slice(0, chamberTop + 1);
 
         rockTop = -1; // null
         rockLeft = -1; // null
         rockHeight = -1; // null
         rockWidth = -1; // null
-
-        if (chamber.length > 900) {
-            const trim = chamber.length - 850;
-            layersTrimmed += trim;
-            chamberTop -= trim;
-
-            chamber = chamber.slice(trim);
-        }
 
         // draw();
         return false;
@@ -227,15 +214,65 @@ function step() {
     return true;
 }
 
-let rocksStopped = 0;
-while (rocksStopped < 2022) {
-    addRock();
-    while (step()) {}
-    rocksStopped++;
+let rocks = 0;
+let heightOffset = 0;
+let cycleFound = false;
+
+const cache = {};
+
+const TARGET_ROCKS = 1e12; // 2022
+
+function checkForCycle() {
+    const offsets = [];
+    for (let x = 0; x < CHAMBER_WIDE; x++) {
+        let y = chamberTop;
+        while (y >= 0) {
+            if (chamber[y][x] === '#') {
+                offsets.push(chamberTop - y);
+                break;
+            }
+            y--;
+        }
+        if (offsets.length !== x + 1) {
+            offsets.push(chamberTop + 1);
+        }
+    }
+
+    const key = [...offsets, ' ', rockCursor, jetCursor].join('|');
+
+    const height = chamberTop + 1;
+    const data = { height, rocks };
+
+    // cycle found
+    if (cache[key]) {
+        const deltaHeight = height - cache[key].height;
+        const deltaRocks = rocks - cache[key].rocks;
+
+        const jumpCycles = Math.floor((TARGET_ROCKS - rocks) / deltaRocks);
+
+        // make the jump
+        cycleFound = true;
+        rocks = rocks + jumpCycles * deltaRocks;
+        heightOffset = jumpCycles * deltaHeight;
+
+        return;
+    }
+
+    cache[key] = data;
 }
 
-draw();
+while (rocks < TARGET_ROCKS) {
+    addRock();
+    while (step()) {}
+    rocks++;
 
-console.log(layersTrimmed + chamberTop + 1);
+    if (!cycleFound) {
+        checkForCycle();
+    }
+}
+
+// draw();
+
+console.log(heightOffset + chamberTop + 1); // 3090 / 1530057803453
 
 console.log('End');
