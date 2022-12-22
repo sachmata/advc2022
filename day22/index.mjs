1d94d6ab5ec294b6eac18997f3eb2aec1b643146
Martin Dimitrov <martin@sachmata.com>
2022-12-22 11:16:52 +0200

    Day 22 draw path

--- day22/index.mjs
+++ day22/index.mjs
@@ -9,7 +9,7 @@ let mapEnd = false;
 const map = [];
 const commands = [];
 
-for (let line of fromFile('./day22/example.txt')) {
+for (let line of fromFile('./day22/input.txt')) {
     if (!line.length) {
         mapEnd = true;
         continue;
@@ -30,18 +30,31 @@ for (let line of fromFile('./day22/example.txt')) {
     map.push(tiles);
 }
 
-// const DIRECTIONS = {
-//     R: 0, // right
-//     D: 1, // down
-//     L: 2, // left
-//     U: 3, // up
-// };
+const DIRECTIONS = {
+    R: 0, // right
+    D: 1, // down
+    L: 2, // left
+    U: 3, // up
+};
+
+const MARKS = ['>', 'v', '<', '^'];
+
+const mark = direction => MARKS[direction];
+
+const draw = () => {
+    for (let y = 0; y < map.length; y++) {
+        console.log(map[y].join(''));
+    }
+    console.log('\n');
+};
 
 let posY = 0;
 let posX = map[0].findIndex(t => t !== ' ');
 let direction = 0;
 
 console.log(posX, posY, direction);
+
+map[posY][posX] = mark(direction);
 
 for (const [steps, rotation] of commands) {
     console.log(steps, rotation);
@@ -50,16 +63,19 @@ for (const [steps, rotation] of commands) {
     const dY = direction === 1 ? 1 : direction === 3 ? -1 : 0;
 
     for (let i = 0; i < steps; i++) {
-        let nextTile = map[posY + dY]?.[posX + dX] ?? null;
-
-        if (nextTile === '.') {
-            posX += dX;
-            posY += dY;
-            continue;
-        }
+        let nextTile = map[posY + dY]?.[posX + dX] ?? ' ';
 
         if (nextTile === '#') {
             break;
+        }
+
+        if (nextTile === '.' || MARKS.includes(nextTile)) {
+            posX += dX;
+            posY += dY;
+
+            map[posY][posX] = mark(direction);
+
+            continue;
         }
 
         // out of map
@@ -68,30 +84,39 @@ for (const [steps, rotation] of commands) {
         let _posX = posX;
         let _posY = posY;
 
-        let _prevTile = map[_posY + ndY]?.[_posX + ndX] ?? null;
-        while (_prevTile === '.' || _prevTile === '#') {
+        let _prevTile = map[_posY + ndY]?.[_posX + ndX] ?? ' ';
+        while (_prevTile === '.' || MARKS.includes(_prevTile) || _prevTile === '#') {
             _posX += ndX;
             _posY += ndY;
-            _prevTile = map[_posY + ndY]?.[_posX + ndX] ?? null;
+            _prevTile = map[_posY + ndY]?.[_posX + ndX] ?? ' ';
         }
 
-        if (map[_posY][_posX] === '#') {
+        nextTile = map[_posY][_posX];
+
+        if (nextTile === '#') {
             break;
         }
 
-        if (map[_posY][_posX] === '.') {
+        if (nextTile === '.' || MARKS.includes(nextTile)) {
             posX = _posX;
             posY = _posY;
+
+            map[posY][posX] = mark(direction);
+
             continue;
         }
     }
 
     direction = (direction + rotation) % 4;
 
+    map[posY][posX] = mark(direction);
+
     console.log(posX, posY, direction);
 }
 
+draw();
+
 const password = 1e3 * (posY + 1) + 4 * (posX + 1) + direction;
-console.log(password);
+console.log(password); // 51258 ???
 
 console.log('End');
